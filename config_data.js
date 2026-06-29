321dd705de9fcc34faa67b2cd528175e07d71917
uiusasdevelopment <uiusasdevelopment@gmail.com>
2026-06-28 21:03:17 -0300

    Update from user prompt

--- config_data.js
+++ config_data.js
@@ -10,12 +10,66 @@ window.INITIAL_CONFIG = {
 ],
     presentations: [
     {
-        "id": "pres_1782691147258",
+        "id": "pres_1782691304550",
         "title": "Simulado prático - HE Digestório (1).pdf (1)",
         "subtitle": "",
-        "coverUrl": "media/thumb-1782691147255-335250040-Simulado prático - HE Digestório (1).pdf (1).pdf.jpg",
+        "coverUrl": "media/thumb-1782691304547-745934600-Simulado prático - HE Digestório (1).pdf (1).pdf.jpg",
+        "bgColor": "linear-gradient(135deg, #833ab4, #fd1d1d, #fcb045)",
+        "fileUrl": "media/Simulado prÃ¡tico - HE DigestÃ³rio (1).pdf (1)-1782691304519-124731912.pdf",
+        "slides": [
+            {
+                "title": "Capa",
+                "content": "Título da Capa"
+            },
+            {
+                "title": "Slide 1",
+                "content": "Conteúdo do primeiro slide"
+            }
+        ]
+    },
+    {
+        "id": "pres_1782691324206",
+        "title": "Malária",
+        "subtitle": "",
+        "coverUrl": "media/thumb-1782691324200-388622364-Malária.pdf.jpg",
+        "bgColor": "linear-gradient(135deg, #11998e, #38ef7d)",
+        "fileUrl": "media/MalÃ¡ria-1782691324183-462273579.pdf",
+        "slides": [
+            {
+                "title": "Capa",
+                "content": "Título da Capa"
+            },
+            {
+                "title": "Slide 1",
+                "content": "Conteúdo do primeiro slide"
+            }
+        ]
+    },
+    {
+        "id": "pres_1782691335310",
+        "title": "Amebíase",
+        "subtitle": "",
+        "coverUrl": "media/thumb-1782691335307-160818008-Amebíase.pdf.jpg",
+        "bgColor": "linear-gradient(135deg, #11998e, #38ef7d)",
+        "fileUrl": "media/AmebÃ­ase-1782691335293-442018198.pdf",
+        "slides": [
+            {
+                "title": "Capa",
+                "content": "Título da Capa"
+            },
+            {
+                "title": "Slide 1",
+                "content": "Conteúdo do primeiro slide"
+            }
+        ]
+    },
+    {
+        "id": "pres_1782691354595",
+        "title": "HTLV-1 e HTLV-2",
+        "subtitle": "",
+        "coverUrl": "media/thumb-1782691354590-458686371-HTLV-1 e HTLV-2.pdf.jpg",
         "bgColor": "linear-gradient(135deg, #1fa2ff, #12d8fa, #a6ffcb)",
-        "fileUrl": "media/Simulado prÃ¡tico - HE DigestÃ³rio (1).pdf (1)-1782691147229-339114848.pdf",
+        "fileUrl": "media/HTLV-1 e HTLV-2-1782691354565-455425045.pdf",
         "slides": [
             {
                 "title": "Capa",
